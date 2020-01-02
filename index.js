402a596f797468833830417e582654098fbd3273
BoyLondon <fucshu@163.com>
2020-01-02 21:29:31 +0800

    header finished

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-var app = new Vue({
-    el: '#app',
+var header = new Vue({
+    el: '#header',
     data: {
-        message: 'Hello Vu'
+        message: '还有: '
     }
-});
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: '页面加载于 ' + new Date().toLocaleString()
-    }
-    });
+})
